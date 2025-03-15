f228affdadd4a5d64481604fd150bd87a9bc46d8
emilyfrancis2003 <emilyfrancis2003@gmail.com>
2025-03-16 02:03:32 +0800

    ef_5

--- lib/Page/Interview.dart
+++ lib/Page/Interview.dart
@@ -25,12 +25,12 @@ class Interview extends StatelessWidget {
     },
     {
       'company': 'OpenAI - Research Engineer',
-      'image': 'job2.jpg',
+      'image': 'job4.jpg',
       'sessions': '2'
     },
     {
       'company': 'Microsoft - AI Engineer',
-      'image': 'job3.jpg',
+      'image': 'job5.png',
       'sessions': '2'
     },
   ];
@@ -40,39 +40,41 @@ class Interview extends StatelessWidget {
     return Scaffold(
       appBar: TopNavBar(),
       backgroundColor: Color(0xFFFFF5EC),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Practice Mock Interview for Any Role",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Implement navigation to a detailed interview list
-                  },
-                  child: Text(
-                    "view more",
-                    style: TextStyle(
-                        color: Colors.orange,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Practice Mock Interview for Any Role",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Expanded(
-              child: GridView.builder(
+                  TextButton(
+                    onPressed: () {
+                      // Implement navigation to a detailed interview list
+                    },
+                    child: Text(
+                      "view more",
+                      style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3,
-                  crossAxisSpacing: 30,
-                  mainAxisSpacing: 30,
+                  crossAxisSpacing: 35,
+                  mainAxisSpacing: 35,
                   childAspectRatio: 1.5,
                 ),
                 itemCount: interviewData.length,
@@ -80,8 +82,9 @@ class Interview extends StatelessWidget {
                   return _buildInterviewCard(interviewData[index]);
                 },
               ),
-            ),
-          ],
+              SizedBox(height: 40),
+            ],
+          ),
         ),
       ),
     );
@@ -107,7 +110,6 @@ class Interview extends StatelessWidget {
           Expanded(
             child: Stack(
               children: [
-                // Background image
                 ClipRRect(
                   borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
                   child: Image.asset(
@@ -116,35 +118,24 @@ class Interview extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                // Blur effect
                 Positioned.fill(
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius:
                           BorderRadius.vertical(top: Radius.circular(30)),
-                      color: Colors.black
-                          .withOpacity(0.3), // Semi-transparent overlay
+                      color: Colors.black.withOpacity(0.4),
                     ),
                   ),
                 ),
-                // Company name
                 Positioned(
-                  bottom: 10,
-                  left: 10,
-                  right: 10,
+                  left: 15,
+                  top: 20,
                   child: Text(
                     data['company']!,
                     style: TextStyle(
                       color: Colors.white,
-                      fontSize: 18,
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 5,
-                          color: Colors.black.withOpacity(0.5),
-                          offset: Offset(2, 2),
-                        ),
-                      ],
                     ),
                   ),
                 ),
@@ -152,32 +143,35 @@ class Interview extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Text(
                       data['sessions']!,
                       style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                     ),
                     Text("Interview Sessions Required"),
                   ],
                 ),
-                CircleAvatar(
-                  radius: 27,
-                  backgroundColor: Color(0xFF49BBBD),
-                  child: Icon(Icons.play_arrow, color: Colors.white),
-                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    shape: const CircleBorder(),
+                    padding: EdgeInsets.all(25),
+                    backgroundColor: Color(0xFF49BBBD),
+                  ),
+                  child: Icon(Icons.play_arrow, color: Colors.white, size: 30),
+                )
               ],
             ),
           ),
         ],
       ),
     );
-   
   }
 }
